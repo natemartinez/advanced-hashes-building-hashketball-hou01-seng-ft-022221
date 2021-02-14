d78ad29f1b6cfa0a21d33c5669b9df2403d4f6d9
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-02-14 21:46:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,11 @@
 def game_hash 
 
-
-
+{
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
 player_name: "Alan Anderson",
           number: 0,
           shoe: 16,
